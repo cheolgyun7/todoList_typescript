01ce48c2c64eab302413bf361cfe176bdfec2d13
Kim CG <cjfrbs1256@gmail.com>
2024-03-05 20:12:08 +0900

    add: level1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { randomUUID } from "crypto";
+import React, { useState } from "react";
+// import "./App.css";
+
+interface Todo {
+  id: number;
+  name: string;
+  constent: string;
+}
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [name, setName] = useState("");
+  const [constent, setConstent] = useState("");
+  console.log(todos);
+  const handleSubmit = () => {
+    let newTodos: Todo = {
+      id: todos.length + 1,
+      name,
+      constent,
+    };
+    setTodos([...todos, newTodos]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="이름"
+      />
+      <br />
+      <input
+        value={constent}
+        onChange={(e) => setConstent(e.target.value)}
+        placeholder="내용"
+      />
+      <button onClick={handleSubmit}>저장</button>
     </div>
   );
 }
